2e3b887649fe6840a02a8f3e4cb6ce5df5742462
Priya <sreepdsy1994@gmail.com>
2022-02-11 22:26:15 -0500

    Add package for get and set methods

---
+++ src/getAndSetMethods/Employee.java
@@ -0,0 +1,34 @@
+package getAndSetMethods;
+
+public class Employee {
+	
+	String name;
+	int empId;
+	double salary;
+	
+	//get method -- to get or retrieve the value of a variable
+	//set method -- to update the value of a variable
+	
+	String getName() {
+		return name;
+	}
+	void setName(String newName){
+		name = newName;
+	}
+	
+	int getEmpId() {
+		return empId;
+	}
+	
+	void setEmpId(int newEmpId) {
+		empId = newEmpId;
+	}
+	
+	double getSalary() {
+		return salary;
+	}
+	
+	void setSalary(double newSalary) {
+		salary = newSalary;
+	}
+}
---
+++ src/getAndSetMethods/MainEmployee.java
@@ -0,0 +1,35 @@
+package getAndSetMethods;
+
+import java.util.Scanner;
+
+public class MainEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Employee emp = new Employee();
+
+		System.out.println("Directly calling the variable using object reference");
+		emp.name = "Neeru";
+		System.out.println("Name :" + emp.name);
+
+		System.out.println("Calling method to update the variable Name");
+		emp.setName("Kamal");
+		System.out.println("Name :" + emp.getName());
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//updating and retrieving value of salary variable
+		System.out.println("Setting the salary");
+		
+		System.out.println("Enter the salary");
+		double empSalary = sc.nextDouble();	//setting the user input value
+		emp.setSalary(empSalary);
+		System.out.println("The employee salary is " + emp.getSalary());	//retrieving using get method
+		
+		System.out.println("Enter employee ID");
+		emp.setEmpId(sc.nextInt());	//setting user input 
+		System.out.println("Updated Salary is " + emp.getEmpId());	//retrieving the set value
+	}
+
+}
